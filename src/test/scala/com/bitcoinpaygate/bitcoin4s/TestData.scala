49f58b5a75f40e9513471c053d6e26a14c28afe3
lukasz.drygala <lukasz.drygala@gmail.com>
2018-08-21 08:20:46 +0200

    fix typo

--- src/test/scala/com/bitcoinpaygate/bitcoin4s/TestData.scala
+++ src/test/scala/com/bitcoinpaygate/bitcoin4s/TestData.scala
@@ -9,7 +9,7 @@ object TestData {
   val miningInfoResponse = readJson("mining-info-response.json")
   val memPoolInfoResponse = readJson("mem-pool-info-response.json")
   val blockchainInfoResponse = readJson("blockchain-info-response.json")
-  val estimateFeeResponse = readJson("esitmate-fee-response.json")
+  val estimateFeeResponse = readJson("estimate-fee-response.json")
   val listUnspentResponse = readJson("list-unspent-response.json")
   val listAccountsResponse = readJson("list-accounts-response.json")
   val getNewAddressResponse = readJson("get-new-address-response.json")
